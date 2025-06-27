57012ed42a41a6e7011309d6b01bbed92d1b9382
Ademir2021 <centroserra@gmail.com>
2025-06-27 13:12:58 -0300

    upd code

--- src/components/sale/SaleComponet.tsx
+++ src/components/sale/SaleComponet.tsx
@@ -1,16 +1,18 @@
 import type { Item } from "../../models/items"
+import type { Order } from "../../models/orderItems"
 
 import "./styles.css"
 
 
 type Props = {
     items: Item[]
-    handleUpItem:Function
-    handleDownItem:Function
+    order:Order
+    handleUpItem: Function
+    handleDownItem: Function
 }
 
 const SaleComponet: React.FC<Props> = ({
-    items, handleUpItem, handleDownItem
+    items, order, handleUpItem, handleDownItem
 }: Props) => {
 
     const items_ = <>
@@ -20,23 +22,28 @@ const SaleComponet: React.FC<Props> = ({
             <ul className="items-container-ul">
                 {items.map((item, index) => (
                     <li className="items-container-li" key={index}>
-                        <b>Item: </b>{item.id} {item.name}
-                        <p>
-                            <b>Marca: </b>{item.brand.name}
-                            {" "}
-                            <b>Setor: </b>{item.sector.name}
-                        </p>
-                        <p><b>Preco Min</b>: R$ {item.priceMin}
-                            {" "}<b>Preco Max</b>: R$ {item.priceMax}</p>
-                        <p><b>Imagem: </b>{item.imagem}
-                            <br /> <b>Barras: </b>{item.barCode}</p>
-                            <button onClick={() => handleUpItem(item)}>Incluir Item</button>
-                            <button onClick={() => handleDownItem(item)}>Remover Item</button>
+                        <p><b>Item </b>{item.id} {item.name}</p>
+                        <p><b>Marca </b>{item.brand.name}</p>
+                        <p><b>Setor </b>{item.sector.name}</p>
+                        <p><b>Valor </b>{item.priceMax}</p>
+                        <form><input placeholder="Informe a Quantidade" /></form>
+                        <button onClick={() => handleUpItem(item)}>Incluir Item</button>
+                        <button onClick={() => handleDownItem(item)}>Remover Item</button>
                     </li>))}</ul></div></>
+    
+    const order_ = <>    {<div>{order.itemSale.map((item, index) => (
+      <div key={index}>
+        <p >{item.item.id}</p>
+        <p >{item.item.name}</p>
+        <p >{item.price}</p>
+        <p >{item.tItem}</p>
+      </div>
+    ))}</div>}</>
 
     return <>
         <h2>Sale</h2>
         <>{items_}</>
+        <>{order_}</>
     </>
 }
 
--- src/pages/Sale.tsx
+++ src/pages/Sale.tsx
@@ -5,87 +5,100 @@ import api from '../services/api/api';
 import type { ItemsSale, Order } from '../models/orderItems';
 
 const Sale: React.FC = () => {
-const [items, setItems] = useState<Item[]>([])
-const [order, setOrder] = useState<Order>({
-person: { id: 1 },
-branch: { id: 1 },
-user: { id: 1 },
-tSale:0,
-tNote:0,
-discount: 2,
-itemSale: []
-})
+  const [items, setItems] = useState<Item[]>([])
+  const [order, setOrder] = useState<Order>({
+    person: { id: 1 },
+    branch: { id: 1 },
+    user: { id: 1 },
+    tSale: 0,
+    tNote: 0,
+    discount: 2,
+    itemSale: []
+  })
 
-useEffect(() => {
-api.get<Item[]>("items/items")
-.then(response => {
-setItems(response.data)
-})
-},[])
+  async function getSorageOrder() {
+    const storeOrder = localStorage.getItem("sale")
+    if (storeOrder)
+      setOrder(JSON.parse(storeOrder))
+  }
 
-function sumItem(item: ItemsSale) {
-for (let i of order.itemSale)
-if (i.item.id === item.item.id) {
-i.amount = item.amount + item.amount;
-return item.tItem = item.amount * item.price;
-}
-}
+  useEffect(() => {
+    getSorageOrder()
+  }, [order])
 
-function verifItem(item: ItemsSale) {
-const exists = order.itemSale.some(i => i.item.id === item.item.id);
-if (exists) {
-alert('Este item já foi adicionado à venda!');
-return;
-} else {
-order.itemSale.push(item)
-sumTotalSale()
-setOrder(order)
-console.log(order)
-}
-}
 
-function sumTotalSale(){
-order.tSale = 0.00
-for (let i of order.itemSale)
-order.tSale += i.tItem
-order.tNote = order.tSale - order.discount
-}
+  useEffect(() => {
+    api.get<Item[]>("items/items")
+      .then(response => {
+        setItems(response.data)
+      })
+  }, [])
 
-const handleUpItem = (item: Item) => {
+  function sumItem(item: ItemsSale) {
+    for (let i of order.itemSale)
+      if (i.item.id === item.item.id) {
+        i.amount = item.amount + item.amount;
+        return item.tItem = item.amount * item.price;
+      }
+  }
 
-const setItemOrder: ItemsSale = {
-item: { id: 0, name: "" },
-amount: 0,
-price: 0,
-tItem: 0
-}
-setItemOrder.item.id = item.id
-setItemOrder.item.name = item.name
-setItemOrder.amount = 2
-setItemOrder.price = item.priceMax
-setItemOrder.tItem = setItemOrder.price * setItemOrder.amount
-sumItem(setItemOrder)
-verifItem(setItemOrder)
-};
+  function verifItem(item: ItemsSale) {
+    const exists = order.itemSale.some(i => i.item.id === item.item.id);
+    if (exists) {
+      alert('Este item já foi adicionado à venda!');
+      return;
+    } else {
+      order.itemSale.push(item)
+      sumTotalSale()
+      setOrder(order)
+    }
+  }
 
-const handleDownItem = (item: Item) => {
-const newArray = order.itemSale.filter(obj => obj.item.id !== item.id);
-order.itemSale = newArray
-sumTotalSale()
-console.log(order)
-};
+  function sumTotalSale() {
+    order.tSale = 0.00
+    for (let i of order.itemSale)
+      order.tSale += i.tItem
+    order.tNote = order.tSale - order.discount
+  }
 
-function handleSubmit() {
-console.log(order)
-}
+  const handleUpItem = (item: Item) => {
 
-return <>
-<SaleComponet
-items={items}
-handleUpItem={handleUpItem}
-handleDownItem={handleDownItem}
-/>
-<button onClick={handleSubmit}>Finalizar venda</button>
-</>
+    const setItemOrder: ItemsSale = {
+      item: { id: 0, name: "" },
+      amount: 0,
+      price: 0,
+      tItem: 0
+    }
+    setItemOrder.item.id = item.id
+    setItemOrder.item.name = item.name
+    setItemOrder.amount = 2
+    setItemOrder.price = item.priceMax
+    setItemOrder.tItem = setItemOrder.price * setItemOrder.amount
+    sumItem(setItemOrder)
+    verifItem(setItemOrder)
+    localStorage.setItem("sale", JSON.stringify(order))
+  };
+
+  const handleDownItem = (item: Item) => {
+    const newArray = order.itemSale.filter(obj => obj.item.id !== item.id);
+    order.itemSale = newArray
+    sumTotalSale()
+    localStorage.setItem("sale", JSON.stringify(order))
+  };
+
+  function handleSubmit() {
+    setOrder(order)
+  }
+
+  return <>
+    <SaleComponet
+      items={items}
+      order={order}
+      handleUpItem={handleUpItem}
+      handleDownItem={handleDownItem}
+    />
+
+    <button onClick={handleSubmit}>Finalizar venda</button>
+  </>
 }
 export default Sale;
